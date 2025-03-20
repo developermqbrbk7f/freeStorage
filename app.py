203a90937b4b0687ffa9f2639c8ca092bb14d88d
developermqbrbk7f <semipah291@gmail.com>
2025-03-20 17:12:53 +0100

    First version

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+PASSWORD_FILE = 'passwords.json'
+
+def load_passwords():
+    try:
+        with open(PASSWORD_FILE, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+def save_passwords(passwords):
+    with open(PASSWORD_FILE, 'w') as f:
+        json.dump(passwords, f, indent=4)
+
+@app.route('/')
+def index():
+    passwords = load_passwords()
+    return render_template('index.html', passwords=passwords)
+
+if __name__ == '__main__':
+    app.run(debug=True)
